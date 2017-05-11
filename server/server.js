301933a0d526eea55f00db541b3423074f50a4ea
BIOLYTIC\ut.duong <ut.duong@biolytic.com>
2017-05-11 15:41:31 -0700

    Setup seperate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +9,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -92,7 +93,7 @@ app.patch('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
         res.send({todo});
-        
+
     }).catch((e) => {
         res.status(400).send();
     });
