ad8f8d86d1c5b25e4b8083ae70584489936a34fc
BIOLYTIC\ut.duong <ut.duong@biolytic.com>
2017-05-11 18:14:47 -0700

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -19,8 +19,8 @@ app.post('/todos', (req, res) => {
     });
     todo.save().then((doc) => {
         res.send(doc)
-    }).catch((e) => {
-        res.status(400).send();
+    }, (e) => {
+        res.status(400).send(e);
     });
 });
 
@@ -96,6 +96,20 @@ app.patch('/todos/:id', (req, res) => {
 
     }).catch((e) => {
         res.status(400).send();
+    });
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
     });
 });
 
